16a973c5e324e9f7cac0a1590dc50e5b0eefff47
Bashir Shafii <bashir.shafii@gmail.com>
2018-07-12 09:28:10 -0500

    created persona for the first user

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>
+			Joe's favorite concert venues
+		</title>
+	</head>
+	<body>
+		<img src="joe.jpg" alt="Joe's photo" width="150" height="150">
+		<ul>
+		<li>Name : Joe </li>
+		<li>Age : 52</li>
+		<li>Gender : Male</li>
+		<li>Profession : College Professor</li>
+		<li>Attitudes : Loves going to country music concerts. </li>
+		</ul>
+		
+	</body>
+
+</html>
